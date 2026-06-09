03f4f7a5b2ac0fe64dee1bb569fec6271ca796d6
David Piranishvili <piranishvili2005@gmail.com>
2026-06-09 17:25:41 +0400

    task-1.4-1.5

--- Vention.API/Program.cs
+++ Vention.API/Program.cs
@@ -1,3 +1,5 @@
+using Vention.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddOptions<CryptoSettingsOptions>()
+    .Bind(builder.Configuration.GetSection("CryptoSettings"))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +25,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection();
 
---
+++ Vention.Application/User/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Vention.Application;
+using Vention.Application.User.Services;
+
+
+namespace Vention.Week1.Application.User.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly CryptoSettingsOptions _cryptoSettings;
+
+        public UserService(IOptions<CryptoSettingsOptions> cryptoSettings)
+        {
+            _cryptoSettings = cryptoSettings.Value;
+        }
+
+
+        public string HashPassword(string password)
+        {
+            string passwordWithPepper = password + _cryptoSettings.PasswordPepper;
+
+            using var sha256 = System.Security.Cryptography.SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
