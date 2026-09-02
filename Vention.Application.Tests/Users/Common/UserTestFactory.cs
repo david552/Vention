7b13be5be5e8baff4ebe9aad561510380bdf5ed5
David Piranishvili <piranishvili2005@gmail.com>
2026-09-02 12:13:21 +0400

    tasks-13.1-13.7

---
+++ Vention.Application.Tests/Users/Common/UserTestFactory.cs
@@ -0,0 +1,14 @@
+﻿using Vention.Domain.Users;
+
+namespace Vention.Application.Tests.Users.Common
+{
+
+    internal static class UserTestFactory
+    {
+        public static User Create(
+            string email = "user@example.com",
+            string name = "Test User",
+            string passwordHash = "hashed-password")
+            => User.Create(Email.Create(email), name, passwordHash);
+    }
+}
